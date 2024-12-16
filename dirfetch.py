513250087ec0eb6f4a89dcf1826908c563db809a
herb-chan <wujowujo3@gmail.com>
2024-12-16 09:07:36 +0100

    Fixed pywal integration on windows

--- dirfetch.py
+++ dirfetch.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import fnmatch
 import json
+import platform
 from datetime import datetime
 from collections import defaultdict
 from rich.console import Console
@@ -28,12 +29,25 @@ def load_config(config_file):
 
 # Function to load colors from pywal's colors.json
 def load_pywal_colors():
+    os_name = platform.system()
+
+    if os_name == "Windows":
+        user = os.getlogin()
+        colors_path = os.path.expanduser(f"C:\\Users\\{user}\\.cache\\wal\\colors.json")
+    else:
+        colors_path = os.path.expanduser("~/.cache/wal/colors.json")
+
     try:
-        with open(os.path.expanduser("~/.cache/wal/colors.json"), "r") as file:
+        with open(colors_path, "r", encoding="utf-8") as file:
             colors = json.load(file)
             return colors['colors']  # Extract the 'colors' dictionary
+        
     except FileNotFoundError:
         print("Pywal colors file not found. Using default colors.")
+        return {f"color{i}": "#FFFFFF" for i in range(0, 16)}  # Fallback to white for all colors
+    
+    except json.JSONDecodeError as e:
+        print(f"JSONDecodeError: {e.msg} at line {e.lineno} column {e.colno}")
         return {f"color{i}": "#FFFFFF" for i in range(0, 16)}  # Fallback to white for all colors
 
 # Format file size in a human-readable format
