405ed409f3a5079ced0672dbd61e3d7ea6ec17ad
herb-chan <wujowujo3@gmail.com>
2024-12-21 14:16:41 +0100

    Update to version 1.0.24

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name="dirfetch",
-    version="1.0.23",
+    version="1.0.24",
     description="Dirfetch",
     author="herb",
     py_modules=["main"],  # Specify main.py as the module
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        "rich",
+    ],
     entry_points={
         "console_scripts": [
             "dirfetch=main:main",  # Adjusted to import directly from main.py
