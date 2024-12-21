4432c6cafefa6fbe6e63244a05a4ce80c2b4e50b
herb-chan <wujowujo3@gmail.com>
2024-12-21 13:04:57 +0100

    Update to version 1.0.16

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="dirfetch",
-    version="1.0.15",
-    packages=find_packages(where="source"),
+    version="1.0.16",
+    packages=find_packages(),
     package_dir={"": "source"},  # Corrects where the modules are
     include_package_data=True,
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "dirfetch = source.main:main",  # Correct module path
+            "dirfetch = source.main:main",
         ],
     },
 )
