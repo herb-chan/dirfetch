7913d702df468509a41114e66abd4ccdd68e5521
herb-chan <wujowujo3@gmail.com>
2024-12-21 13:12:10 +0100

    Update to version 1.0.19

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="dirfetch",
-    version="1.0.18",
+    version="1.0.19",
     packages=find_packages("source"),
     package_dir={"": "source"},  # Map the root of the packages to "source"
     include_package_data=True,
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "dirfetch = main:main",  # Directly reference the `main` function in `main.py`
+            "dirfetch = source:main",  # Directly reference the `main` function in `main.py`
         ],
     },
 )
